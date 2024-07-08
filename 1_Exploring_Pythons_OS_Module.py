c2493a4def498e9e2ec1078596d15a9294e33140
MArney86 <matthew.h.arney.1986@gmail.com>
2024-07-07 20:49:37 -0500

    finalize code

--- 1_Exploring_Pythons_OS_Module.py
+++ 1_Exploring_Pythons_OS_Module.py
@@ -6,11 +6,12 @@
 #Problem Statement: Create a Python script that lists all files and subdirectories in a given directory. Your script should prompt the user for the directory path and then display the contents.
 
 import os
-import re
 
 def list_subdir(path, counter):
     tabs = "\t" * counter
+    
     print(f"\033[7m{tabs}{path}:\033[0m")
+    
     for object in os.listdir(path):
         fullpath = os.path.join(path,object)
         if os.path.isdir(fullpath):
@@ -20,8 +21,8 @@ def list_subdir(path, counter):
             print(f"{tabs}{object}")
 
 def list_directory_contents(path):
-    print(f"\033[7m{path}:\033[0m")
     try:
+        print(f"\n\033[7m{path}:\033[0m")
         for object in os.listdir(path):
             fullpath = os.path.join(path,object)
             if os.path.isdir(fullpath):
@@ -29,9 +30,27 @@ def list_directory_contents(path):
             else:
                 print(object)
     except FileNotFoundError:
-        print("That directory does not exist, please try again")
+        print("\n\033[7mThat directory does not exist, please try again\033[0m")
     except PermissionError:
-        print("You do not currently have permission to access this directory")
+        print("\n\033[7mYou do not currently have permission to access this directory\033[0m")
+    except IOError:
+        print("\n\033[7mAn unexpected IOError has occurred\033[0m")
     except:
-        print("An unexpected error has occurred")
-list_directory_contents("Test_Directory")
+        print("\n\033[7mAn unexpected error has occurred\033[0m")
+
+while True:
+    print("\nDirectory Inspector - List all files and subdirectories of a directory")
+    print("1: Inspect the provided test directory")
+    print("2: Provide your own directory to Inspect")
+    print("3: Exit")
+    choice = input("Which function would you like to execute?: ")
+
+    if choice == '1':
+        list_directory_contents("Test_Directory")
+    elif choice == '2':
+        user_dir = input("What is the full path to the directory you'd like to Inspect?: ")
+        list_directory_contents(user_dir)
+    elif choice == '3':
+        break
+    else:
+        print("\033[7mThat was not a valid choice, please try again\033[0m")
